a1a2078fc6407bb1aae8f59bee25cb4703f8af37
yyo6420 <yyo6420@gmail.com>
2025-12-31 13:13:28 +0200

    utils/mysql.js

---
+++ utils/mysql.js
@@ -0,0 +1,47 @@
+import mysql from "mysql2/promise";
+
+export async function makeSQLDatabase() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    port: 3306,
+  });
+
+  const CREATE_DB_QUERY = `CREATE DATABASE IF NOT EXISTS ecommerce;`;
+
+  const USE_DB_QUERY = "USE ecommerce;";
+
+  const CREATE_TABLE_QUERY = `
+  CREATE TABLE IF NOT EXISTS orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    productId VARCHAR(24) NOT NULL,
+    quantity INT NOT NULL,
+    customerName VARCHAR(255) NOT NULL,
+    totalPrice DECIMAL(10,2) NOT NULL,
+    orderDate DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+`;
+
+  await connection.query(CREATE_DB_QUERY);
+  await connection.query(USE_DB_QUERY);
+  await connection.query(CREATE_TABLE_QUERY);
+
+  await connection.end();
+}
+
+let singletoneConnection = null;
+
+export async function makeConnection() {
+  if (singletoneConnection) return singletoneConnection;
+  else {
+    singletoneConnection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      port: 3306,
+      database: "ecommerce",
+    });
+    return singletoneConnection;
+  }
+}
